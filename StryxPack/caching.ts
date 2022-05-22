3e6ce341c3fba6abc8b34e7626189618070a2ad1
Connor Shearer <connor_shearer@outlook.com>
2022-05-22 06:15:11 +0100

    Change cache testing to use sha256 and not datetimes

--- StryxPack/caching.ts
+++ StryxPack/caching.ts
@@ -1,27 +1,33 @@
 import { join } from 'path';
-import { stat } from 'fs/promises';
+import { readFile } from 'fs/promises';
+import crypto from 'crypto';
 
 import { fileExists, __dirname } from './utilities';
 import { cachedManifest } from './manifests';
-import { setHasUpdateQueued } from './globals';
+import { setHasUpdateQueued, __client_wwwrootdev_dirname, __client_wwwroot_dirname } from './globals';
 
-export async function testCache(__client_wwwroot_dirname: string, __client_wwwrootdev_dirname: string, itempath: string, outExt: string)
+export async function testCache(itempath: string, outExt: string)
 {
     const entryExists = cachedManifest.manifest.map(x => x.path).indexOf(itempath) > -1;
     var shouldCache = true;
+    const sha = crypto.createHash('sha256');
+    sha.setEncoding('hex');
+    sha.write(await readFile(itempath));
+    sha.end();
+    const hash = sha.read();
     if (entryExists)
     {
         const entry = cachedManifest.manifest.filter(file => file.path === itempath)[0];
         const outFileExists = await fileExists(
             outExt == 'min.css' ? join(__client_wwwroot_dirname, 'bundle.min.css') :
                 outExt == 'min.js' ? join(__client_wwwroot_dirname, 'bundle.min.js') :
-                    itempath.replace(__client_wwwrootdev_dirname, __client_wwwroot_dirname).substring(0, itempath.lastIndexOf('.') - 4) + '.' + outExt).catch(err => console.error(err));
-        shouldCache = outFileExists ? new Date(entry.lastModified).getTime() < (await stat(itempath).then(data => data).catch(err => console.error(err))).mtime.getTime() : true;
+                    itempath.replace(__client_wwwrootdev_dirname, __client_wwwroot_dirname).substring(0, itempath.lastIndexOf('.') - 4) + '.' + outExt);
+        shouldCache = outFileExists ? entry.hash !== hash : true;
     }
     if (shouldCache)
     {
         if (cachedManifest.manifest.map(x => x.path).indexOf(itempath) > -1) cachedManifest.manifest.splice(cachedManifest.manifest.map(x => x.path).indexOf(itempath), 1);
-        cachedManifest.manifest.push({ path: itempath, lastModified: (await stat(itempath)).mtime });
+        cachedManifest.manifest.push({ path: itempath, hash: hash });
         setHasUpdateQueued(true);
     }
     return shouldCache;
