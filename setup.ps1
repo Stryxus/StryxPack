1f635fce3dfaff234a79bbf903424c1dbe2f78a5
Connor Shearer <connor_shearer@outlook.com>
2022-07-11 08:58:37 +0100

    Need to add in dart pub get as well

--- setup.ps1
+++ setup.ps1
@@ -6,4 +6,6 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
  }
 }
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
-choco install dart-sdk 
+choco install dart-sdk
+cd StryxPack
+dart pub get
